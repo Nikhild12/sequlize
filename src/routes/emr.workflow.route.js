91564af27c9a57e09048dd1b0fd0b6599fd6d3d0
Murugan <murugan@oasyscybernetics.in>
2019-11-07 10:35:04 +0530

    Removed Prescription Js and commentted EMR Workflow Route

--- src/routes/emr.workflow.route.js
+++ src/routes/emr.workflow.route.js
@@ -1,10 +1,14 @@
 // package Imports
 const express = require('express');
 
+// EMR Workflow Controller Import
 const emrWorkflowController = require('../controllers/emr.workflow.controller');
 
+// Express Router Initialize
 const emrWorkflowRoute = express.Router();
 
+// EMR Workflow Routes
 emrWorkflowRoute.route('/create').post(emrWorkflowController.createEMRWorkFlow);
 
+// Exporting EMR Workflow Route
 module.exports = emrWorkflowRoute;
