df453a176b0f284315530f23e7fc1b97f54c2dd0
madhusai <madhusai@oasyscybernetics.in>
2019-12-09 18:36:18 +0530

    getHistoryPatientVitals API

--- src/controllers/emr.patient.vitals.controller.js
+++ src/controllers/emr.patient.vitals.controller.js
@@ -6,8 +6,12 @@ var Op = Sequelize.Op;
 
 const sequelizeDb = require('../config/sequelize');
 
+const emrConstants = require('../config/constants');
 // Initialize EMR Workflow
 const emr_patientvitals_Tbl = sequelizeDb.patient_vitals;
+
+// Initialize EMR Views
+const vw_patientVitalsTbl = sequelizeDb.vw_patient_vitals;
 
 
 const EMRPatientVitals = () => {
@@ -60,21 +64,38 @@ const EMRPatientVitals = () => {
     }
 
     const _getPatientVitals = async (req, res) => {
-        
+
         try {
-            
+
             let getPatientVitals = await emr_patientvitals_Tbl.findAll({ where: { is_active: 1, status: 1 } }, { returning: true });
             return res.status(200).send({ code: httpStatus.OK, message: "Fetched EMR Patient Vital Details  Successfully", responseContents: getPatientVitals });
         }
-        catch(ex){
+        catch (ex) {
             return res.status(400).send({ code: httpStatus[400], message: ex.message });
         }
-    }    
+    }
+    const _getHistoryPatientVitals = async (req, res) => {
+        const { user_uuid } = req.headers;
+        const { patient_uuid, department_uuid } = req.query;
+        if (user_uuid && patient_uuid && department_uuid) {
 
+            try {
+                let getHistoryPatientVitals = await vw_patientVitalsTbl.findAll(getHistoryPatientVitalQuery(user_uuid, patient_uuid, department_uuid), { returning: true });
+                return res.status(200).send({ code: httpStatus.OK, message: "Fetched EMR History Patient Vital Details  Successfully", responseContents: patientVitalsList(getHistoryPatientVitals) });
+            }
+            catch (ex) {
+                return res.status(400).send({ code: httpStatus[400], message: ex.message });
+            }
+        }
+        else {
+            return res.status(400).send({ code: httpStatus[400], message: "No Request Params Found" });
+        }
+    }
     return {
         createPatientVital: _createPatientVital,
         getVitalsByTemplateID: _getVitalsByTemplateID,
-        getPatientVitals:_getPatientVitals,
+        getPatientVitals: _getPatientVitals,
+        getHistoryPatientVitals: _getHistoryPatientVitals,
 
     }
 }
@@ -83,3 +104,41 @@ const EMRPatientVitals = () => {
 
 module.exports = EMRPatientVitals();
 
+function getHistoryPatientVitalQuery(user_uuid, patient_uuid, department_uuid) {
+    // user_uuid == doctor_uuid
+    let query = {
+        order: [['pv_uuid', 'DESC']],
+        attributes: ['pv_uuid', 'pv_vital_master_uuid', 'pv_vital_type_uuid', 'pv_vital_value_type_uuid', 'pv_vital_value', 'pv_doctor_uuid', 'pv_patient_uuid', 'pv_performed_date', 'vm_name', 'um_code', 'um_name'],
+        limit: 10,
+        where: { vm_active: 1, vm_status: 1, pv_doctor_uuid: user_uuid, pv_patient_uuid: patient_uuid, pv_department_uuid: department_uuid },
+    }
+    return query;
+}
+
+function patientVitalsList(getHistoryPatientVitals) {
+    let patient_vitals_list = [];
+    if (getHistoryPatientVitals && getHistoryPatientVitals.length > 0) {
+
+        getHistoryPatientVitals.forEach((pV) => {
+            patient_vitals_list = [...patient_vitals_list,
+            {
+                vital_name: pV.vm_name,
+                vital_value: pV.pv_vital_value,
+                vital_performed_date: pV.pv_performed_date,
+                vital_value_type_uuid: pV.pv_vital_value_type_uuid,
+                vital_type_uuid: pV.pv_vital_type_uuid,
+                vital_master_uuid: pV.pv_vital_master_uuid,
+                patient_vital_uuid: pV.pv_uuid,
+
+                uom_code: pV.um_code,
+                uom_name: pV.um_name,
+
+                patient_uuid: pV.pv_patient_uuid,
+                doctor_uuid: pV.pv_doctor_uuid,
+
+            }
+            ]
+        });
+    }
+    return patient_vitals_list;
+}
---
+++ src/views/vw_patient_vitals.js
@@ -0,0 +1,101 @@
+module.exports = (sequelize, DataTypes) => {
+    const vw_patient_vitals = sequelize.define(
+        "vw_patient_vitals",
+        {
+            vm_name: {
+                type: DataTypes.STRING(100),
+            },
+            vm_active:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+            vm_status:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+            um_code:{
+                type: DataTypes.STRING(50),
+            },
+            um_name:{
+                type: DataTypes.STRING(50),
+            },
+            um_active:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+            um_status:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+            pv_uuid:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true
+            },
+            pv_facility_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_department_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_patient_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_encounter_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_encounter_type_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_doctor_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_consultation_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_group_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_type_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_master_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_qualifier_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_value_type_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_vital_value:{
+                type: DataTypes.STRING(50),
+            },
+            pv_vital_uom_uuid:{
+                type: DataTypes.INTEGER,
+            },
+            pv_performed_date:{
+                type: DataTypes.DATE,
+            },
+            pv_active:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+            pv_status:{
+                type: DataTypes.ENUM,
+                values: ["0", "1"],
+                defaultValue: "1"
+            },
+        },
+        {
+            freezeTableName: true,
+        }
+    );
+
+    return vw_patient_vitals;
+}
