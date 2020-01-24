5ee52b6f9970378e12fa576ba2f903ba0ce1b3f0
Murugan <murugan@oasyscybernetics.in>
2020-01-24 14:15:38 +0530

    Json Changed as per Mobile requirement

--- src/config/emr_mock_json.js
+++ src/config/emr_mock_json.js
@@ -363,135 +363,308 @@ const _patientVisitHistoryJson = [
 
 const _patientChiefComplaintsJson = [
   {
-    patient_chief_complaint_id: 1,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    chief_complaint_id: 1,
-    chief_complaint_name: "Fever",
-    chief_complaint_code: "Fever",
-    chief_complaint_duration_id: 1,
-    vital_value_type_name: "Days",
-    chief_complaint_duration_code: "Days"
+    doctor_uuid: 1,
+    doctor_name: "Murugan",
+    department_uuid: 1,
+    department_name: "Orthopeadic",
+    data: [
+      {
+        patient_chief_complaint_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "Fever",
+        chief_complaint_code: "Fever",
+        chief_complaint_duration_id: 1,
+        vital_value_type_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "cold",
+        chief_complaint_code: "cold",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "dengue",
+        chief_complaint_code: "dengue",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 5,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "diarrhoea",
+        chief_complaint_code: "diarrhoea",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 6,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "malaria",
+        chief_complaint_code: "malaria",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      }
+    ]
   },
   {
-    patient_chief_complaint_id: 2,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    chief_complaint_id: 1,
-    chief_complaint_name: "cold",
-    chief_complaint_code: "cold",
-    chief_complaint_duration_id: 1,
-    chief_complaint_duration_name: "Days",
-    chief_complaint_duration_code: "Days"
-  },
-  {
-    patient_chief_complaint_id: 3,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    chief_complaint_id: 1,
-    chief_complaint_name: "dengue",
-    chief_complaint_code: "dengue",
-    chief_complaint_duration_id: 1,
-    chief_complaint_duration_name: "Days",
-    chief_complaint_duration_code: "Days"
-  },
-  {
-    patient_chief_complaint_id: 5,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    chief_complaint_id: 1,
-    chief_complaint_name: "diarrhoea",
-    chief_complaint_code: "diarrhoea",
-    chief_complaint_duration_id: 1,
-    chief_complaint_duration_name: "Days",
-    chief_complaint_duration_code: "Days"
-  },
-  {
-    patient_chief_complaint_id: 6,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    chief_complaint_id: 1,
-    chief_complaint_name: "malaria",
-    chief_complaint_code: "malaria",
-    chief_complaint_duration_id: 1,
-    chief_complaint_duration_name: "Days",
-    chief_complaint_duration_code: "Days"
+    doctor_uuid: 2,
+    doctor_name: "Bala",
+    department_uuid: 2,
+    department_name: "Peditaric",
+    data: [
+      {
+        patient_chief_complaint_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "Fever",
+        chief_complaint_code: "Fever",
+        chief_complaint_duration_id: 1,
+        vital_value_type_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "cold",
+        chief_complaint_code: "cold",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "dengue",
+        chief_complaint_code: "dengue",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 5,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "diarrhoea",
+        chief_complaint_code: "diarrhoea",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      },
+      {
+        patient_chief_complaint_id: 6,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        chief_complaint_id: 1,
+        chief_complaint_name: "malaria",
+        chief_complaint_code: "malaria",
+        chief_complaint_duration_id: 1,
+        chief_complaint_duration_name: "Days",
+        chief_complaint_duration_code: "Days"
+      }
+    ]
   }
 ];
 
 const _patientVitalsJson = [
   {
-    patient_vitals_id: 1,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    vital_id: 1,
-    vital_name: "SUGAR",
-    vital_code: "SUGAR",
-    vital_value_type_id: 1,
-    vital_value_type_name: "Image",
-    vital_value_type_code: "Image"
+    doctor_uuid: 1,
+    doctor_name: "Murugan",
+    department_uuid: 1,
+    department_name: "Orthopeadic",
+    data: [
+      {
+        patient_vitals_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "SUGAR",
+        vital_code: "SUGAR",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Image",
+        vital_value_type_code: "Image"
+      },
+      {
+        patient_vitals_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "BP",
+        vital_code: "BP",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Image",
+        vital_value_type_code: "Image"
+      },
+      {
+        patient_vitals_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "WHITE BLOOD",
+        vital_code: "WHITE BLOOD",
+        vital_value_type_id: 1,
+        vital_value_type_name: "NUMBER",
+        vital_value_type_code: "NUMBER"
+      },
+      {
+        patient_vitals_id: 5,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "BMI",
+        vital_code: "BMI",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Days",
+        vital_value_type_code: "Days"
+      }
+    ]
   },
   {
-    patient_vitals_id: 2,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    vital_id: 1,
-    vital_name: "BP",
-    vital_code: "BP",
-    vital_value_type_id: 1,
-    vital_value_type_name: "Image",
-    vital_value_type_code: "Image"
-  },
-  {
-    patient_vitals_id: 3,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    vital_id: 1,
-    vital_name: "WHITE BLOOD",
-    vital_code: "WHITE BLOOD",
-    vital_value_type_id: 1,
-    vital_value_type_name: "NUMBER",
-    vital_value_type_code: "NUMBER"
-  },
-  {
-    patient_vitals_id: 5,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    vital_id: 1,
-    vital_name: "BMI",
-    vital_code: "BMI",
-    vital_value_type_id: 1,
-    vital_value_type_name: "Days",
-    vital_value_type_code: "Days"
+    doctor_uuid: 2,
+    doctor_name: "Madhu",
+    department_uuid: 2,
+    department_name: "Orthopeadic",
+    data: [
+      {
+        patient_vitals_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "SUGAR",
+        vital_code: "SUGAR",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Image",
+        vital_value_type_code: "Image"
+      },
+      {
+        patient_vitals_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "BP",
+        vital_code: "BP",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Image",
+        vital_value_type_code: "Image"
+      },
+      {
+        patient_vitals_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "WHITE BLOOD",
+        vital_code: "WHITE BLOOD",
+        vital_value_type_id: 1,
+        vital_value_type_name: "NUMBER",
+        vital_value_type_code: "NUMBER"
+      },
+      {
+        patient_vitals_id: 5,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        vital_id: 1,
+        vital_name: "BMI",
+        vital_code: "BMI",
+        vital_value_type_id: 1,
+        vital_value_type_name: "Days",
+        vital_value_type_code: "Days"
+      }
+    ]
   }
 ];
 
 const _patientDiagnosisJson = [
   {
-    patient_vitals_id: 1,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    diagnosis_id: 1,
-    diagnosis_name: "Typhoid meningitis",
-    diagnosis_code: "Typhoid meningitis",
-    diagnosis_value_type_id: 1
+    doctor_uuid: 1,
+    doctor_name: "Murugan",
+    department_uuid: 1,
+    department_name: "Orthopeadic",
+    data:[
+      {
+        patient_vitals_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 1,
+        diagnosis_name: "Typhoid meningitis",
+        diagnosis_code: "Typhoid meningitis",
+        diagnosis_value_type_id: 1
+      },
+      {
+        patient_diagnosis_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 2,
+        diagnosis_name: "Typhoid fever",
+        diagnosis_code: "Typhoid fever",
+        diagnosis_value_type_id: 1
+      },
+      {
+        patient_diagnosis_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 3,
+        diagnosis_name: "Cholera, unspecified",
+        diagnosis_code: "Cholera, unspecified"
+      }
+    ]
   },
   {
-    patient_diagnosis_id: 2,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    diagnosis_id: 2,
-    diagnosis_name: "Typhoid fever",
-    diagnosis_code: "Typhoid fever",
-    diagnosis_value_type_id: 1
-  },
-  {
-    patient_diagnosis_id: 3,
-    encounter_id: 192,
-    encounter_type_id: 1,
-    diagnosis_id: 3,
-    diagnosis_name: "Cholera, unspecified",
-    diagnosis_code: "Cholera, unspecified"
+    doctor_uuid: 2,
+    doctor_name: "Madhu",
+    department_uuid: 1,
+    department_name: "Peditaric",
+    data:[
+      {
+        patient_vitals_id: 1,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 1,
+        diagnosis_name: "Typhoid meningitis",
+        diagnosis_code: "Typhoid meningitis",
+        diagnosis_value_type_id: 1
+      },
+      {
+        patient_diagnosis_id: 2,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 2,
+        diagnosis_name: "Typhoid fever",
+        diagnosis_code: "Typhoid fever",
+        diagnosis_value_type_id: 1
+      },
+      {
+        patient_diagnosis_id: 3,
+        encounter_id: 192,
+        encounter_type_id: 1,
+        diagnosis_id: 3,
+        diagnosis_name: "Cholera, unspecified",
+        diagnosis_code: "Cholera, unspecified"
+      }
+    ]
   }
 ];
 
