4f6bd8482999b436169941f04d73be120e56e6c2
keerthana <keerthana@oasys.co>
2020-02-10 19:11:17 +0530

    models

--- src/models/immunizationSchedule.js
+++ src/models/immunizationSchedule.js
@@ -3,84 +3,6 @@
 module.exports = (sequelize, DataTypes) => {
     const immunization_schedule = sequelize.define(
         "immunization_schedule", {
-<<<<<<< Updated upstream
-        uuid: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-
-        schedule_uuid: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        immunization_uuid: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        immunization_name: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        immunization_schedule_flag_uuid: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        immunization_route_uuid: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        immunization_dosage_uuid: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        duration: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        immunization_period_uuid: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        display_order: {
-            type: DataTypes.STRING(250),
-            allowNull: true
-        },
-        status: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: 1
-        },
-        revision: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: 1
-
-        },
-        is_active: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: 1
-        },
-        created_by: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-
-        modified_by: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-
-    }, {
-        tableName: "immunization_schedule",
-        createdAt: 'created_date',
-        updatedAt: 'modified_date',
-        indexes: [{
-            fields: ["uuid"]
-        }]
-    }
-=======
             uuid: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
@@ -155,7 +77,6 @@ module.exports = (sequelize, DataTypes) => {
                 fields: ["uuid"]
             }]
         }
->>>>>>> Stashed changes
     );
 
     return immunization_schedule;
