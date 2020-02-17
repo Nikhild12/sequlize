13e30f19709b5dcd0c627eeda0c433ae3b8d12e6
Sangavi <sangavi@oasyscybernetics.in>
2020-02-17 10:48:49 +0530

    cccmaster type

--- src/controllers/cccmaster.controller.js
+++ src/controllers/cccmaster.controller.js
@@ -46,38 +46,21 @@ const cccMasterController = () => {
             sortOrder = getsearch.sortOrder;
         }
         let findQuery = {
-            // offset: offset,
-            // limit: itemsPerPage,
-            // order: [
-            //     [sortField, sortOrder],
-            // ],
+            offset: offset,
+            limit: itemsPerPage,
+            order: [
+                [sortField, sortOrder],
+            ],
             where: { is_active: 1, status: 1 },
             include: [
                 {
                     model: criticalcareTypeTbl,
                     as: 'critical_care_types'
-                    // as: "parent_health_office",
-                    // attributes: ['uuid', 'name']
+                },
+                {
+                    model: conceptTbl,
+                    include: [{ model: conceptdetailsTbl }]
                 }
-                // include : [
-
-                //     { model: bio_waste_detail_tbl ,
-
-                //     include : [ {model: bin_code_master_tbl,include : [ {model: bin_code_color_tbl}]
-
-                //     }]},
-                //     // where:{is_active:1,status:1}}, {
-                //     {model: bio_med_waste_status_tbl
-
-                //     }]
-                // {
-                //     model: conceptTbl,
-                //     attributes: ['uuid','concept_name','value_type_uuid','is_default']
-                // },
-                // {
-                //     model: conceptdetailsTbl,
-                //     attributes: ['uuid', 'concept_values','value_from','value_to']
-                // },
             ]
         };
         if (getsearch.search && /\S/.test(getsearch.search)) {
@@ -118,11 +101,10 @@ const cccMasterController = () => {
             if (typeof req.body != "object" || Object.keys(req.body).length < 1) {
             }
             const { user_uuid, facility_uuid } = req.headers;
-
+            let concept_detail_output;
             let transaction;
 
             let transaction_status = false;
-            let patient_orders_output_uuid = null;
             try {
 
                 transaction = await db.sequelize.transaction();
@@ -133,10 +115,6 @@ const cccMasterController = () => {
                 const postDatabody1 = req.body.body1;
                 const postDatabody2 = req.body.body2;
 
-
-                postDatabody.is_active = 1;
-                postDatabody.status = 1;
-                postDatabody.revision = 1;
                 postDatabody.created_by = user_uuid;
                 postDatabody.modified_by = 0;
                 postData.created_by = req.headers.user_uuid
@@ -147,38 +125,40 @@ const cccMasterController = () => {
                 let ccc_master_output = await cccMasterTbl.create(postDatabody, { returning: true, transaction });
 
                 postDatabody1.cc_chart_uuid = ccc_master_output.dataValues.uuid
-                // postDatabody1.concept_name = ccc_master_output.name
-                // postDatabody1.concept_code = ccc_master_output.code
-                // postDatabody1.is_multiple = 1;
-                // postDatabody1.is_default = 1;
-                // postDatabody1.display_order = 1;
-                // postDatabody1.is_active = 1;
-                // postDatabody1.status = 1;
-                // postDatabody1.revision = 1;
                 postDatabody1.created_by = user_uuid;
                 postDatabody1.modified_by = 0;
                 postDatabody1.created_by = req.headers.user_uuid
-                let concept_output = await conceptTbl.create(postDatabody1, { returning: true, transaction })
-                console.log(concept_output, 'concept');
+                let concept_output = await conceptTbl.create(postDatabody1, { returning: true, transaction });
 
+                let finalCConceptData = [];
 
-                postDatabody2.forEach((pD) => {
-                    pD.cc_concept_uuid = concept_output.uuid
-                    // pD.display_order = 1;
-                    // pD.is_default = 1;
-                    // pD.is_active = 1;
-                    // pD.status = 1;
-                    // pD.revision = 1;
-                    pD.created_by = user_uuid;
-                    pD.modified_by = 0;
-                    pD.created_by = req.headers.user_uuid
-                    // pD.concept_value = 44;
+                postDatabody2.forEach(e => {
+                    let obj = {};
+                    obj.cc_concept_uuid = concept_output.dataValues.uuid;
+                    if (e.concept_value.length > 0) {
+                        e.concept_value.forEach(e2 => {
+                            let obj_copy = JSON.parse(JSON.stringify(obj));
+                            obj_copy.concept_value = e2.term
+                            finalCConceptData.push(obj_copy);
+                        })
+                    } else {
+                        let obj_copy = JSON.parse(JSON.stringify(obj));
+                        obj_copy.value_from = e.value_from1;
+                        obj_copy.value_to = e.value_to1;
+                        obj_copy.value_from = e.value_from2;
+                        obj_copy.value_to = e.value_to2;
+                        obj_copy.value_from = e.value_from3;
+                        obj_copy.value_to = e.value_to3;
+                        finalCConceptData.push(obj_copy);
+                    }
                 });
-                let concept_detail_output = await conceptdetailsTbl.bulkCreate(postDatabody2, { returning: true, transaction });
 
+                // if (finalCConceptData.length > 0) {
+                //     concept_detail_output = await conceptdetailsTbl.bulkCreate(finalCConceptData, { returning: true, transaction });
+                // }
                 await transaction.commit();
                 transaction_status = true;
-                res.send({ statusCode: 200, message: "Created Successfully" })
+                res.send({ statusCode: 200, message: "Created Successfully", responseContents: finalCConceptData })
             } catch (err) {
                 console.log(err)
                 if (transaction) await transaction.rollback();
@@ -189,66 +169,6 @@ const cccMasterController = () => {
                     await transaction.rollback();
                 }
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // let transaction;
-            // let transaction_status = false;
-            // try {
-            //     transaction = await db.sequelize.transaction();
-            //     const postData = req.body;
-            //     const postDatabody1 = req.body.body1;
-            //     const postDatabody2 = req.body.body2;
-            //     postData.created_by = req.headers.user_uuid
-            //     postData.code = postData.fieldname
-            //     postData.name = postData.fieldname
-            //     if (postData) {
-            //         await cccMasterTbl.create(postData, { returning: true,transaction })
-            //             .then(async (data) => {
-            //                 postData.cc_chart_uuid = data.uuid
-            //                 postDatabody1.code = postData.fieldname
-            //                 postDatabody1.name =postData.fieldname
-            //                 await conceptTbl.create(postData, { returning: true, transaction })
-            //                     .then(async (result) => {
-            //                         postData.cc_concept_uuid = result.uuid
-            //                         await conceptdetailsTbl.create(postData, { returning: true, transaction })
-            //                             .then(async (details) => {
-            //                                 await transaction.commit();
-            //                                 transaction_status = true;
-            //                                 res.send({ statusCode: 200, message: "Inserted concept details successfully", responseContents: details, transaction })
-            //                             })
-            //                     }).catch((locationErr) => {
-            //                         await transaction.rollback();
-            //                         transaction_status = true;
-            //                         res.send({ statusCode: 500, msg: "failed to insert concept details", error: locationErr })
-            //                     })
-            //             }).catch(err => {
-            //                 console.log("cccMasterTbl error",err);
-            //                 res.status(500).send({ statusCode: 500, msg: "failed to insert Ccc Master details", error: err })
-            //             })
-            //     }
-            //     else {
-            //         res.send({ status: 'failed', msg: 'Please enter Ccc Master details' })
-            //     }
-            // } catch (error) {
-
-            // }
-
         } catch (err) {
             console.log(err);
             const errorMsg = err.errors ? err.errors[0].message : err.message;
@@ -256,56 +176,7 @@ const cccMasterController = () => {
         }
 
     };
-    // const postcccMaster = async (req, res, next) => { const postData = req.body;
-    //     postData.created_by = req.headers.user_uuid;
-    //     if (postData) {
-    //         cccMasterTbl.findAll({
-    //             where: {
-    //               [Op.or]: [{
-    //                 code: postData.code
-    //                 },
-    //                 {
-    //                    name: postData.name
-    //                 }
-    //               ]
-    //             }
-    //           }).then(async (result) =>{
-    //             if (result.length != 0) {
-    //                 return res.send({
-    //                     statusCode: 400,
-    //                   status: "error",
-    //                   msg: "Record already Found. Please enter ccc Master"
-    //                 });
-    //               } else{
-    //                 await cccMasterTbl.create(postData, {
-    //                     returning: true
-    //                 }).then(data => {
 
-    //                     res.send({
-    //                         statusCode: 200,
-    //                         msg: "Inserted ccc Master details Successfully",
-    //                         req: postData,
-    //                         responseContents: data
-    //                     });
-    //                 }).catch(err => {
-
-    //                     res.send({
-    //                         status: "failed",
-    //                         msg: "failed to ccc Master details",
-    //                         error: err
-    //                     });
-    //                 });
-    //               }
-    //           });
-
-
-    //     } else {
-
-    //         res.send({
-    //             status: 'failed',
-    //             msg: 'Please enter ccc Master details'
-    //         });
-    //     }};
     const deletecccMaster = async (req, res, next) => {
         const postData = req.body;
 
@@ -383,10 +254,9 @@ const cccMasterController = () => {
         }
     };
     const getcccMasterByType = async (req, res, next) => {
-        // console.log("inside api.........")
         const postData = req.body;
         const criticalId = [];
-        const concepId =[];
+        const concepId = [];
         if (!postData.type) {
             return res
                 .status(httpStatus.INTERNAL_SERVER_ERROR)
@@ -419,14 +289,14 @@ const cccMasterController = () => {
             conceptTbl_data.rows.forEach(e1 => {
                 concepId.push(e1.dataValues.uuid)
             })
-            let conceptDetails_Tab =await conceptdetailsTbl.findAndCountAll({
-                where:{
-                    cc_concept_uuid:{
-                        [Op.in]:concepId
+            let conceptDetails_Tab = await conceptdetailsTbl.findAndCountAll({
+                where: {
+                    cc_concept_uuid: {
+                        [Op.in]: concepId
                     }
                 }
             })
-            return res.send({conceptDetails_Tab:conceptDetails_Tab, criticalType: criticalType, ccMaster: ccMaster, conceptTbl_data: conceptTbl_data });
+            return res.send({ conceptDetails_Tab: conceptDetails_Tab, criticalType: criticalType, ccMaster: ccMaster, conceptTbl_data: conceptTbl_data });
         } catch (err) {
             console.log(err);
             const errorMsg = err.errors ? err.errors[0].message : err.message;
