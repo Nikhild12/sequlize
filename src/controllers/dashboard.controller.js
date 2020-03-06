c29a6a3928e31c3ac076bb42a6eba3d7d89103af
chandramouli <chandramouli@osyscybernetics.in>
2020-03-06 17:11:53 +0530

    updated for session and and all three filters

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -27,7 +27,7 @@ const EmrDashBoard = () => {
 
   const _getDashBoard = async (req, res) => {
     const { user_uuid } = req.headers;
-    const { depertment_Id, from_date, to_date, gender } = req.query;
+    const { depertment_Id, from_date, to_date, gender, session } = req.query;
     let patient_uuids = [];
 
     let filterQuery = {
@@ -66,17 +66,70 @@ const EmrDashBoard = () => {
         const comp_gender = gettopcomp_gender(allpatientsC.responseContents, allcomp, gender);
         return res.status(200).send({ code: httpStatus.OK, message: 'Fetched Successfully', responseContents: { "Top_Complaints": comp_gender, "Top_Diagnosis": diag_gender } });
 
+      } else if (session) {
+        const alldiag = await getDiagnosis(filterQuery, Sequelize);
+        const allcomp = await getComplaints(filterQuery, Sequelize);
+
+        var PData = Object.keys(alldiag).map(key => {
+          let matchdata = alldiag[key];
+          matchdata = matchdata.patient_uuid;
+
+          return matchdata;
+        });
+
+        var CData = Object.keys(allcomp).map(key => {
+          let matchdata = allcomp[key];
+          matchdata = matchdata.patient_uuid;
+
+          return matchdata;
+        });
+
+        const allpatients = await getallpatientdetials(user_uuid, req.headers.authorization, PData);
+        const allpatientsC = await getallpatientdetials(user_uuid, req.headers.authorization, CData);
+        const diag_gender = gettopdiagnosis_session(allpatients.responseContents, alldiag, session);
+        const comp_gender = gettopcomp_session(allpatientsC.responseContents, allcomp, session);
+        return res.status(200).send({ code: httpStatus.OK, message: 'Fetched Successfully', responseContents: { "Top_Complaints": comp_gender, "Top_Diagnosis": diag_gender } });
+
       } else if (from_date && to_date) {
 
-        console.log("date filter here-----------------");
+        //console.log("date filter here-----------------");
         const allcomp = await getComplaintsbydate(user_uuid, depertment_Id, from_date, to_date, Sequelize);
         const alldiag = await getDiagnosisbydate(user_uuid, depertment_Id, from_date, to_date, Sequelize);
 
-
         return res.status(200).send({ code: httpStatus.OK, message: 'Fetched Successfully', responseContents: { "Top_Complaints": gettopcomp(allcomp), "Top_Diagnosis": gettopdiag(alldiag) } });
+      }
+      else if (session && from_date && to_date && gender) {
 
+        const alldiag = await getDiagnosis(filterQuery, Sequelize);
+        const allcomp = await getComplaints(filterQuery, Sequelize);
 
-      } else {
+        var PData = Object.keys(alldiag).map(key => {
+          let matchdata = alldiag[key];
+          matchdata = matchdata.patient_uuid;
+
+          return matchdata;
+        });
+
+        var CData = Object.keys(allcomp).map(key => {
+          let matchdata = allcomp[key];
+          matchdata = matchdata.patient_uuid;
+
+          return matchdata;
+        });
+
+        const allpatients = await getallpatientdetials(user_uuid, req.headers.authorization, PData);
+        const allpatientsC = await getallpatientdetials(user_uuid, req.headers.authorization, CData);
+        const allcompdate = await getComplaintsbydate(user_uuid, depertment_Id, from_date, to_date, Sequelize);
+        const alldiagdate = await getDiagnosisbydate(user_uuid, depertment_Id, from_date, to_date, Sequelize);
+        const diag_gender = gettopdiagnosis_gender(allpatients.responseContents, alldiagdate, gender);
+        const comp_gender = gettopcomp_gender(allpatientsC.responseContents, allcompdate, gender);
+        const diag_session = gettopdiagnosis_session(allpatients.responseContents, diag_gender, session);
+        const comp_session = gettopcomp_session(allpatientsC.responseContents, comp_gender, session);
+
+        return res.status(200).send({ code: httpStatus.OK, message: 'Fetched Successfully', responseContents: { "Top_Complaints": comp_session, "Top_Diagnosis": diag_session } });
+
+      }
+      else {
         //console.log("----------------");
         const topComplaints = await getComplaints(filterQuery, Sequelize);
         const topDiagnosis = await getDiagnosis(filterQuery, Sequelize);
@@ -116,7 +169,7 @@ async function getallpatientdetials(user_uuid, authorization, PData) {
 }
 
 async function getComplaints(filterQuery, Sequelize) {
-  console.log(filterQuery);
+  //console.log(filterQuery);
   return patient_chief_complaints_tbl.findAll({
     where: filterQuery,
     group: ['chief_complaint_uuid'],
@@ -282,6 +335,61 @@ function gettopcomp_gender(allpatients, topcomp, gender) {
         }
         ];
       }
+    }
+  }
+  return genderdata;
+}
+
+function gettopdiagnosis_session(allpatients, topDiagnosis, session) {
+
+  //console.log ("patient vists --------",allpatients[0].patient_visits.length);
+
+  let genderdata = [];
+  for (let td of topDiagnosis) {
+    for (let ap of allpatients) {
+      //for (let i=0; i< ap.patient_visits.length;i++){
+      if (td.patient_uuid == ap.uuid && ap.patient_visits[0].session_uuid == session) {
+        console.log(td.patient_uuid, ap.uuid, ap.patient_visits[0].session_uuid, session);
+        genderdata = [...genderdata,
+        {
+          session_uuid: ap.patient_visits[0].session_uuid,
+          //patient_diagnosis_id: td.uuid || 0,
+          Count: td.dataValues.Count,
+          diagnosis_name: td.diagnosis && td.diagnosis.name ? td.diagnosis.name : td.other_diagnosis,
+          diagnosis_code: td.diagnosis && td.diagnosis.code ? td.diagnosis.code : td.diagnosis_uuid,
+
+        }
+        ];
+      }
+      //}
+    }
+  }
+  return genderdata;
+
+}
+
+function gettopcomp_session(allpatients, topcomp, session) {
+
+  //console.log ("gender --------",gender);
+
+  let genderdata = [];
+  for (let td of topcomp) {
+    for (let ap of allpatients) {
+      //for (let i=0; i< ap.patient_visits.length;i++){
+      //console.log(td.patient_uuid, ap.uuid, ap.gender_details.uuid, gender);
+      if (td.patient_uuid == ap.uuid && ap.patient_visits[0].session_uuid == session) {
+        console.log(td.patient_uuid, ap.uuid, ap.patient_visits[0].session_uuid, session);
+        genderdata = [...genderdata,
+        {
+          session_uuid: ap.patient_visits[0].session_uuid,
+          //patient_diagnosis_id: td.uuid || 0,
+          Count: td.dataValues.Count,
+          chiefcomplaint_name: td.chief_complaint.name,
+          chiefcomplaint_code: td.chief_complaint.code
+        }
+        ];
+      }
+      //}
     }
   }
   return genderdata;
