df1c2acd7968d028d6f2d3d8d4ddc668405ed5a0
swathi <swathi.s@oasyscybernetics.in>
2020-03-18 18:57:38 +0530

    url precription added

--- src/controllers/patient_treatments.controller.js
+++ src/controllers/patient_treatments.controller.js
@@ -464,7 +464,7 @@ async function getPrevOrderdDiagnosisData(order_id) {
 
 async function getPrevOrderPrescription(user_uuid, facility_uuid, order_id, patient_uuid) {
   //const url = 'https://qahmisgateway.oasyshealth.co/DEVHMIS-INVENTORY/v1/api/prescriptions/getPrescriptionByPatientTreatmentId';
-  //const url = config.wso2InvestUrl + 'prescriptions/getPrescriptionByPatientTreatmentId';
+  const url = config.wso2InvestUrl + 'prescriptions/getPrescriptionByPatientTreatmentId';
 
 
   let options = {
