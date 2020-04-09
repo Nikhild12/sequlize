ebd9a43e1fda52ff9f74b4a843c1493c6d1f928b
Murugan <murugan@oasyscybernetics.in>
2020-04-09 12:53:15 +0530

    Added Strength in Get API's

--- src/views/vw_favourite_master_details.js
+++ src/views/vw_favourite_master_details.js
@@ -5,170 +5,173 @@ module.exports = (sequelize, DataTypes) => {
       tsm_uuid: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        primaryKey: true
+        primaryKey: true,
       },
       tsm_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tsmd_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       im_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       im_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       im_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       im_is_emar: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       dr_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       df_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       dr_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       df_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       df_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       df_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       dp_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       dp_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       dp_name: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       di_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       di_code: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       di_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tsm_dept: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tsm_userid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       tsm_active: {
         type: DataTypes.ENUM,
         values: ["0", "1"],
-        defaultValue: "1"
+        defaultValue: "1",
       },
       tsm_public: {
         type: DataTypes.ENUM,
         values: ["0", "1"],
-        defaultValue: "1"
+        defaultValue: "1",
       },
       tsm_display_order: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       tsmd_duration: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       tsm_favourite_type_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       tsmd_test_master_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+      },
+      tsmd_strength: {
+        type: DataTypes.STRING,
       },
       cc_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       cc_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       cc_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       vm_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       vm_uom: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tsm_status: {
         type: DataTypes.ENUM,
         values: ["0", "1"],
-        defaultValue: "1"
+        defaultValue: "1",
       },
       ltm_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       ltm_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       ltm_description: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tmsd_diagnosis_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       d_code: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       d_name: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       d_description: {
-        type: DataTypes.STRING(500)
+        type: DataTypes.STRING(500),
       },
       tsmd_diet_master_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       tsmd_speciality_sketch_uuid: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
       },
       im_is_active: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       im_status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       cc_is_active: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       cc_status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       vm_is_active: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       vm_status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       ltm_is_active: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       ltm_status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       d_is_active: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       d_status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
       },
       tsm_display_order: {
-        type: DataTypes.INTEGER
-      }
+        type: DataTypes.INTEGER,
+      },
     }
   );
 
