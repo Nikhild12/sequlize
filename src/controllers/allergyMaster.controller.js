31da3a6e919d4a5061a5a57eb16d9bad8b4feec3
keerthana <keerthana@oasys.co>
2020-04-21 04:37:57 -0700

    'getbyallegryid'

--- src/controllers/allergyMaster.controller.js
+++ src/controllers/allergyMaster.controller.js
@@ -164,7 +164,7 @@ if (getsearch.search && /\S/.test(getsearch.search)) {
                     return res.send({
                         statusCode: 400,
                         status: "error",
-                        msg: "Please enter Allergy Master"
+                        msg: "Please enter new Allergy Master"
                     });
                 } else {
                     await allergyMastersTbl.create(postData, {
@@ -269,7 +269,7 @@ if (getsearch.search && /\S/.test(getsearch.search)) {
 
     const getAlleryMasterById = async (req, res, next) => {
         const postData = req.body;
-        
+
         //var getcuDetails = {},getmuDetails={};
         try {
             if (postData.Allergy_id <= 0) {
