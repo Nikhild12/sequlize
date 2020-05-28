9c0a9f83362458e15503f4d71e11fc9ce96bc5bb
puviyarasan <puviyarasan@oasyscybernetics.in>
2020-05-28 17:08:57 +0530

    get Chief Complaints

--- src/controllers/chief_complaints.controller.js
+++ src/controllers/chief_complaints.controller.js
@@ -457,9 +457,9 @@ const ChiefComplaints = () => {
 
     if (getsearch.search && /\S/.test(getsearch.search)) {
       findQuery.where[Op.or] = [
-        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.name')), 'LIKE', '%' + getsearch.searchKey.toLowerCase() + '%'),
-        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.code')), 'LIKE', '%' + getsearch.searchKey.toLowerCase() + '%'),
-        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.description')), 'LIKE', '%' + getsearch.searchKey.toLowerCase() + '%')
+        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.name')), 'LIKE', '%' + getsearch.search.toLowerCase() + '%'),
+        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.code')), 'LIKE', '%' + getsearch.search.toLowerCase() + '%'),
+        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('chief_complaints.description')), 'LIKE', '%' + getsearch.search.toLowerCase() + '%')
 
       ];
     }
