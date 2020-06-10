14f9542545ecdbce2de71e36f03ed505861a1527
Murugan <murugan@oasyscybernetics.in>
2020-06-10 14:52:57 +0530

    added log

--- src/services/utility.service.js
+++ src/services/utility.service.js
@@ -102,6 +102,8 @@ const _postRequest = async (api, headers, data) => {
   console.log({ api });
   console.log("\n");
   console.log({ headers });
+  console.log("\n");
+  console.log({ data });
 
   return new Promise((resolve, reject) => {
     request.post(
