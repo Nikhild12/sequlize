e3a48f7203f986c127c882b8583a957991c19368
mounika <mounika@oasyscybernetics.in>
2020-06-16 11:42:13 +0530

    note_templates changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
-require("dotenv").config();
+//require("dotenv").config();
+require("dotenv").config({ path: __dirname + '/../../.env' });
 
 const envVarsSchema = Joi.object({
   PG_DB_LOGGING: Joi.string().default(0),
