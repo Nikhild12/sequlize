31ded7464bd08b2394183aadc76580a741c6b3d8
keerthana <keerthana@oasys.co>
2020-11-13 13:36:03 +0530

    treatment kit code changes

--- src/controllers/treatment.kit.controller.js
+++ src/controllers/treatment.kit.controller.js
@@ -91,21 +91,21 @@ const TreatMent_Kit = () => {
     //let treatmentTransaction;
     let { treatment_kit, treatment_kit_lab, treatment_kit_drug } = req.body;
     let { treatment_kit_investigation, treatment_kit_radiology, treatment_kit_diagnosis } = req.body;
-    // let options = {
-    //   uri: config.wso2AppUrl + APPMASTER_GET_SCREEN_SETTINGS,
-    //   headers: {
-    //     Authorization: authorization,
-    //     user_uuid: user_uuid
-    //   },
-    //   body: {
-    //     code: 'CLN'
-    //   }
-    // };
-    // screenSettings_output = await emr_utility.postRequest(options.uri, options.headers, options.body);
-    // if (screenSettings_output) {
-    //   replace_value = parseInt(screenSettings_output.suffix_current_value) + emr_constants.IS_ACTIVE;
-    //   treatment_kit.code = screenSettings_output.prefix + replace_value;
-    // }
+    let options = {
+      uri: config.wso2AppUrl + APPMASTER_GET_SCREEN_SETTINGS,
+      headers: {
+        Authorization: authorization,
+        user_uuid: user_uuid
+      },
+      body: {
+        code: 'CLN'
+      }
+    };
+    screenSettings_output = await emr_utility.postRequest(options.uri, options.headers, options.body);
+    if (screenSettings_output) {
+      replace_value = parseInt(screenSettings_output.suffix_current_value) + emr_constants.IS_ACTIVE;
+      treatment_kit.code = screenSettings_output.prefix + replace_value;
+    }
     if (user_uuid && treatment_kit && treatment_kit.name && treatment_kit.name) {
       if (checkTreatmentKit(req)) {
         return res.status(400).send({
