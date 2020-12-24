f35c25e631d4291f89ec470b0815aef16838304c
balachandra <balachandra@oasyscybernetics.in>
2020-12-24 12:33:03 +0530

    Added respose in add patient trasfer

--- src/controllers/patient_transfer.controller.js
+++ src/controllers/patient_transfer.controller.js
@@ -28,8 +28,8 @@ const Patient_Transfer = () => {
         return res.status(404).send({ code: httpStatus.NOT_FOUND, message: `${emr_constants.NO} ${emr_constants.user_uuid} ${emr_constants.FOUND} ${emr_constants.OR} ${emr_constants.NO} ${emr_constants.NO_REQUEST_BODY} ${emr_constants.FOUND}` });
       }
       await assignDefault(patientTransferData, user_uuid);
-      await patientTransferTbl.create(patientTransferData, { returning: true });
-      return res.status(200).send({ code: httpStatus.OK, message: 'Inserted Success' });
+      const data = await patientTransferTbl.create(patientTransferData, { returning: true });
+      return res.status(200).send({ code: httpStatus.OK, message: 'Inserted Success', responseContents: data });
     } catch (ex) {
       console.log('Exception Happened');
       return res.status(400).send({ code: httpStatus.BAD_REQUEST, message: ex.message });
