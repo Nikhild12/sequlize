66fdb273f91dcf8766ce04583dd322dec64d5e36
manikanta <manikanta.k@oasyscybernetics.in>
2020-12-30 14:59:54 +0530

    ward_uuid added in consultation model

--- src/models/section_category_entries.js
+++ src/models/section_category_entries.js
@@ -18,11 +18,6 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.INTEGER,
                 allowNull: true
             },
-            ward_uuid: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: 0
-            },
             encounter_type_uuid: {
                 type: DataTypes.INTEGER,
                 allowNull: true
