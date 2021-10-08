1a952ef90174ab881e6c98aa918f02f989295690
unknown <vignesh@oasyscybernetics.in>
2021-10-08 18:27:21 +0530

    H30-43921 changes in search api for history section values are done by vignesh k

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -7,8 +7,9 @@ const Op = Sequelize.Op;
 
 const history_tbl = sequelizeDb.historys;
 const history_section_tbl = sequelizeDb.history_sections;
-const history_section_concept_tbl = sequelizeDb.history_section_concepts;
-const history_section_concept_value_tbl = sequelizeDb.history_section_concept_values;
+const history_section_values_tbl = sequelizeDb.history_section_values;
+// const history_section_concept_tbl = sequelizeDb.history_section_concepts;
+// const history_section_concept_value_tbl = sequelizeDb.history_section_concept_values;
 const history_category_tbl = sequelizeDb.history_category;
 const history_sub_category_tbl = sequelizeDb.history_sub_category;
 const value_type_tbl = sequelizeDb.value_types;
@@ -80,7 +81,7 @@ const historys = () => {
 
             let findQueryHCSection = {
                 required: false,
-                attributes: ['uuid', 'history_uuid', 'section_name', 'display_order'],
+                attributes: ['uuid', 'history_uuid', 'section_name', 'value_type_uuid', 'display_order'],
                 where: {
                     history_uuid: { [Op.or]: hc_uuid },
                     is_active: 1,
@@ -94,30 +95,11 @@ const historys = () => {
                 return acc;
             }, []);
 
-
-            let findQueryHcSectionConcept = {
-                required: false,
-                attributes: ['uuid', 'concept_name', 'history_section_uuid',
-                    'value_type_uuid', 'is_multiple', 'is_mandatory'],
-                where: {
-                    history_section_uuid: { [Op.or]: hC_section_uuid },
-                    is_active: 1,
-                    status: 1
-                }
-            }
-            const findHcSectionConceptResponse = await history_section_concept_tbl.findAndCountAll(findQueryHcSectionConcept);
-            let hC_section_concept = findHcSectionConceptResponse.rows;
-            let hC_section_concept_uuid = hC_section_concept.reduce((acc, cur) => {
-                acc.push(cur.uuid);
-                return acc;
-            }, []);
-
-            let hC_section_concept_value_type_uuid = hC_section_concept.reduce((acc, cur) => {
+            let hC_section_value_type_uuid = hC_section.reduce((acc, cur) => {
                 acc.push(cur.value_type_uuid);
                 return acc;
             }, []);
-            let uniq_vt_uuid = [... new Set(hC_section_concept_value_type_uuid)];
-
+            let uniq_vt_uuid = [... new Set(hC_section_value_type_uuid)];
 
             let findValueTypeNameQuery = {
                 required: false,
@@ -132,50 +114,97 @@ const historys = () => {
             let value_type_concept_value = findHcSectionValueTypeResponse.rows;
 
 
-            let findQueryHcSectionConceptValues = {
+            // let findQueryHcSectionConcept = {
+            //     required: false,
+            //     attributes: ['uuid', 'concept_name', 'history_section_uuid',
+            //         'value_type_uuid', 'is_multiple', 'is_mandatory'],
+            //     where: {
+            //         history_section_uuid: { [Op.or]: hC_section_uuid },
+            //         is_active: 1,
+            //         status: 1
+            //     }
+            // }
+            // const findHcSectionConceptResponse = await history_section_concept_tbl.findAndCountAll(findQueryHcSectionConcept);
+            // let hC_section_concept = findHcSectionConceptResponse.rows;
+            // let hC_section_concept_uuid = hC_section_concept.reduce((acc, cur) => {
+            //     acc.push(cur.uuid);
+            //     return acc;
+            // }, []);
+
+            // let hC_section_concept_value_type_uuid = hC_section_concept.reduce((acc, cur) => {
+            //     acc.push(cur.value_type_uuid);
+            //     return acc;
+            // }, []);
+            // let uniq_vt_uuid = [... new Set(hC_section_concept_value_type_uuid)];
+
+
+            // let findValueTypeNameQuery = {
+            //     required: false,
+            //     attributes: ['uuid', 'code', 'name'],
+            //     where: {
+            //         uuid: { [Op.or]: uniq_vt_uuid },
+            //         is_active: 1,
+            //         status: 1
+            //     }
+            // };
+            // const findHcSectionValueTypeResponse = await value_type_tbl.findAndCountAll(findValueTypeNameQuery);
+            // let value_type_concept_value = findHcSectionValueTypeResponse.rows;
+
+            // let findQueryHcSectionConceptValues = {
+            //     required: false,
+            //     attributes: ['uuid', 'history_section_concept_uuid',
+            //         'value_name', 'display_order'],
+            //     where: {
+            //         history_section_concept_uuid: { [Op.or]: hC_section_concept_uuid },
+            //         is_active: 1,
+            //         status: 1
+            //     }
+            // }
+            // const findHcSectionConceptValuesResponse = await history_section_concept_value_tbl.findAndCountAll(findQueryHcSectionConceptValues);
+            // let hC_section_concept_value = findHcSectionConceptValuesResponse.rows;
+
+            let findQueryHcSectionValues = {
                 required: false,
-                attributes: ['uuid', 'history_section_concept_uuid',
+                attributes: ['uuid', 'history_section_uuid',
                     'value_name', 'display_order'],
                 where: {
-                    history_section_concept_uuid: { [Op.or]: hC_section_concept_uuid },
+                    history_section_uuid: { [Op.or]: hC_section_uuid },
                     is_active: 1,
                     status: 1
                 }
             }
-            const findHcSectionConceptValuesResponse = await history_section_concept_value_tbl.findAndCountAll(findQueryHcSectionConceptValues);
-            let hC_section_concept_value = findHcSectionConceptValuesResponse.rows;
+            const findHcSectionValuesResponse = await history_section_values_tbl.findAndCountAll(findQueryHcSectionValues);
+            let hC_section_value = findHcSectionValuesResponse.rows;
 
             let concept_and_values_with_no_vt = [];
-            for (let i = 0; i < hC_section_concept.length; i++) {
+            for (let i = 0; i < hC_section.length; i++) {
                 let concept_obj = {
-                    uuid: hC_section_concept[i].uuid,
-                    concept_name: hC_section_concept[i].concept_name,
-                    history_section_uuid: hC_section_concept[i].history_section_uuid,
-                    value_type_uuid: hC_section_concept[i].value_type_uuid,
-                    is_multiple: hC_section_concept[i].is_multiple,
-                    is_mandatory: hC_section_concept[i].is_mandatory,
-                    history_section_concept_value: []
+                    uuid: hC_section[i].uuid,
+                    history_uuid: hC_section[i].history_uuid,
+                    section_name: hC_section[i].section_name,
+                    value_type_uuid: hC_section[i].value_type_uuid,
+                    display_order: hC_section[i].display_order,
+                    history_section_value: []
                 };
-                for (let j = 0; j < hC_section_concept_value.length; j++) {
-                    if (hC_section_concept[i].uuid === hC_section_concept_value[j].history_section_concept_uuid) {
-                        concept_obj.history_section_concept_value.push(hC_section_concept_value[j])
+                for (let j = 0; j < hC_section_value.length; j++) {
+                    if (hC_section_concept[i].uuid === hC_section_value[j].history_section_uuid) {
+                        concept_obj.history_section_value.push(hC_section_value[j])
                     }
                 }
                 concept_and_values_with_no_vt.push(concept_obj)
             }
 
-            let concept_and_values = [];
+            let section_and_values = [];
             for (let i = 0; i < concept_and_values_with_no_vt.length; i++) {
                 let concept_and_vt_obj = {
                     uuid: concept_and_values_with_no_vt[i].uuid,
-                    concept_name: concept_and_values_with_no_vt[i].concept_name,
-                    history_section_uuid: concept_and_values_with_no_vt[i].history_section_uuid,
+                    history_uuid: concept_and_values_with_no_vt[i].history_uuid,
+                    section_name: concept_and_values_with_no_vt[i].section_name,
                     value_type_uuid: concept_and_values_with_no_vt[i].value_type_uuid,
+                    display_order: concept_and_values_with_no_vt[i].display_order,
                     value_type_name: '',
                     value_type_code: '',
-                    is_multiple: concept_and_values_with_no_vt[i].is_multiple,
-                    is_mandatory: concept_and_values_with_no_vt[i].is_mandatory,
-                    history_section_concept_value: concept_and_values_with_no_vt[i].history_section_concept_value
+                    history_section_value: concept_and_values_with_no_vt[i].history_section_value
                 }
                 for (let j = 0; j < value_type_concept_value.length; j++) {
                     if (concept_and_values_with_no_vt[i].value_type_uuid === value_type_concept_value[j].uuid) {
@@ -183,25 +212,48 @@ const historys = () => {
                         concept_and_vt_obj.value_type_code = value_type_concept_value[j].code;
                     }
                 }
-                concept_and_values.push(concept_and_vt_obj);
+                section_and_values.push(concept_and_vt_obj);
             }
 
-            let section_and_concept_values = [];
-            for (let i = 0; i < hC_section.length; i++) {
-                const section_concept = {
-                    uuid: hC_section[i].uuid,
-                    history_uuid: hC_section[i].history_uuid,
-                    section_name: hC_section[i].section_name,
-                    display_order: hC_section[i].display_order,
-                    history_section_concept: []
-                }
-                for (let j = 0; j < concept_and_values.length; j++) {
-                    if (hC_section[i].uuid === concept_and_values[j].history_section_uuid) {
-                        section_concept.history_section_concept.push(concept_and_values[j])
-                    }
-                }
-                section_and_concept_values.push(section_concept)
-            }
+
+            // let concept_and_values = [];
+            // for (let i = 0; i < concept_and_values_with_no_vt.length; i++) {
+            //     let concept_and_vt_obj = {
+            //         uuid: concept_and_values_with_no_vt[i].uuid,
+            //         concept_name: concept_and_values_with_no_vt[i].concept_name,
+            //         history_section_uuid: concept_and_values_with_no_vt[i].history_section_uuid,
+            //         value_type_uuid: concept_and_values_with_no_vt[i].value_type_uuid,
+            //         value_type_name: '',
+            //         value_type_code: '',
+            //         is_multiple: concept_and_values_with_no_vt[i].is_multiple,
+            //         is_mandatory: concept_and_values_with_no_vt[i].is_mandatory,
+            //         history_section_concept_value: concept_and_values_with_no_vt[i].history_section_concept_value
+            //     }
+            //     for (let j = 0; j < value_type_concept_value.length; j++) {
+            //         if (concept_and_values_with_no_vt[i].value_type_uuid === value_type_concept_value[j].uuid) {
+            //             concept_and_vt_obj.value_type_name = value_type_concept_value[j].name;
+            //             concept_and_vt_obj.value_type_code = value_type_concept_value[j].code;
+            //         }
+            //     }
+            //     concept_and_values.push(concept_and_vt_obj);
+            // }
+
+            // let section_and_concept_values = [];
+            // for (let i = 0; i < hC_section.length; i++) {
+            //     const section_concept = {
+            //         uuid: hC_section[i].uuid,
+            //         history_uuid: hC_section[i].history_uuid,
+            //         section_name: hC_section[i].section_name,
+            //         display_order: hC_section[i].display_order,
+            //         history_section_concept: []
+            //     }
+            //     for (let j = 0; j < concept_and_values.length; j++) {
+            //         if (hC_section[i].uuid === concept_and_values[j].history_section_uuid) {
+            //             section_concept.history_section_concept.push(concept_and_values[j])
+            //         }
+            //     }
+            //     section_and_concept_values.push(section_concept)
+            // }
 
             let hC_section_arr = [];
             for (let i = 0; i < hc.length; i++) {
@@ -219,9 +271,9 @@ const historys = () => {
                     comments: hc[i].history.comments,
                     history_section: []
                 };
-                for (let j = 0; j < section_and_concept_values.length; j++) {
-                    if (hc[i].history.uuid === section_and_concept_values[j].history_uuid) {
-                        hC_obj.history_section.push(section_and_concept_values[j])
+                for (let j = 0; j < section_and_values.length; j++) {
+                    if (hc[i].history.uuid === section_and_values[j].history_uuid) {
+                        hC_obj.history_section.push(section_and_values[j])
                     }
                 }
                 hC_section_arr.push(hC_obj);
