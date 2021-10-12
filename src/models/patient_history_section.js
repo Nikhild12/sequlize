217696347fcc48858559883643c75dfe137a244a
unknown <vignesh@oasyscybernetics.in>
2021-10-12 19:05:08 +0530

    H30-44040 Patient history details insert api done by Vignesh K

---
+++ src/models/patient_history_section.js
@@ -0,0 +1,88 @@
+module.exports = function (sequelize, DataTypes) {
+    const patient_history_section = sequelize.define("patient_history_sections",
+        {
+            uuid: {
+                type: DataTypes.INTEGER(11).UNSIGNED,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            patient_history_uuid: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 0,
+            },
+            history_section_uuid: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 0,
+            },
+            history_section_name: {
+                type: DataTypes.STRING(255),
+                allowNull: true
+            },
+            value_type_uuid: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 0,
+            },
+            value_type_name: {
+                type: DataTypes.STRING(255),
+                allowNull: true
+            },
+            comments: {
+                type: DataTypes.STRING(255),
+                allowNull: true
+            },
+            is_active: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: 1
+            },
+            status: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: 1
+            },
+            revision: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 1,
+            },
+            created_by: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 0,
+                validate: {
+                    notNull: true
+                }
+            },
+            modified_by: {
+                type: DataTypes.INTEGER(11),
+                allowNull: false,
+                defaultValue: 0,
+                validate: {
+                    notNull: true
+                }
+            },
+            created_date: {
+                type: DataTypes.DATE,
+                allowNull: true,
+
+            },
+            modified_date: {
+                type: DataTypes.DATE,
+                allowNull: true,
+
+            }
+        }, {
+        tableName: 'patient_history_sections',
+        createdAt: 'created_date',
+        updatedAt: 'modified_date',
+        timestamps: false,
+        indexes: [
+            {
+                fields: ["uuid"]
+            }
+        ]
+    }
+    );
+    return patient_history_section;
+};
