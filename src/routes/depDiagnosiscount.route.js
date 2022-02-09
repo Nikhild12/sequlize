e50e4a679b18e2030b33badc451eec1cc5679def
giftin <giftin.j@oasys.co>
2022-02-09 15:35:35 +0530

    H30-47209 Diagnosis Dashboard and Reports Gender and Diagnosis Count Merge conflict

--- src/routes/depDiagnosiscount.route.js
+++ src/routes/depDiagnosiscount.route.js
@@ -7,6 +7,11 @@ const router = express.Router();
 
 router.route('/depDiagnosis').post(depDiagnosisCtrl.view_depDiagnosis);
 router.route('/docDiagnosis').post(depDiagnosisCtrl.view_docDiagnosis);
+router.route('/docDiagnosisGenderWise').post(depDiagnosisCtrl.view_docDiagnosisGengerwise);
+router.route('/view_docDiagnosisVisitwise').post(depDiagnosisCtrl.view_docDiagnosisVisitwise);
+router.route('/view_docDiagnosiscount').post(depDiagnosisCtrl.view_docDiagnosiscount);
+
+
 
 
 module.exports = router;
