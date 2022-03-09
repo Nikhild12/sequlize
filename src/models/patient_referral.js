56138a2323fe7957b5f730b7ea5d859bbe4452c0
Sreeni <sreeni@oasyscybernetics.in>
2022-03-09 16:28:16 +0530

    While referring patient from General medicine to cardiology for Second opinion, it doesn't reflect from the review notes or patient past history

--- src/models/patient_referral.js
+++ src/models/patient_referral.js
@@ -92,6 +92,22 @@ module.exports = (sequelize, DataTypes) => {
           }
         }
       },
+      consultation_uuid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: emr_constants.GetpleaseProvideMsg('consultation_uuid')
+          },
+          notEmpty: {
+            msg: emr_constants.GetpleaseProvideMsg('consultation_uuid')
+          },
+          min: {
+            args: 1,
+            msg: emr_constants.GetZeroValidationMessage('consultation_uuid')
+          }
+        }
+      },
       referred_by: {
         type: DataTypes.INTEGER,
         allowNull: false,
